0cdfd8c331c5e262f54cf59100f1090892801bfc
leticia <lechi_20@hotmail.com>
2021-02-26 21:34:24 +0100

    segunda revisión

--- Miproyectojava/src/Holamundo.java
+++ Miproyectojava/src/Holamundo.java
@@ -4,7 +4,8 @@ public class Holamundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hola Mundo");
-
+		
+		System.out.print("Mi nombre es Leticia");
 	}
 
 }
